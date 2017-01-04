419042eff1f81962a86c23fc902210605ce8e3c3
Fernando Pinho <fpinho@gmail.com>
2017-01-04 17:40:40 -0200

    test(app): update application for automate test

--- app/src/main/kotlin/io/clappr/player/app/PlayerActivity.kt
+++ app/src/main/kotlin/io/clappr/player/app/PlayerActivity.kt
@@ -1,49 +1,89 @@
 package io.clappr.player.app
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
 import io.clappr.player.Player
 import io.clappr.player.base.Callback
+import io.clappr.player.base.ErrorInfo
 import io.clappr.player.base.Event
 import io.clappr.player.base.Options
 import io.clappr.player.log.Logger
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+
 
 class PlayerActivity : Activity() {
+
+    lateinit var player: Player
+    lateinit var mediaUrl: EditText
+    lateinit var playerState: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
 
-        val player = Player()
-        player.configure(Options(source = "http://clappr.io/highline.mp4", autoPlay = false))
-        player.on(Event.WILL_PLAY.value, Callback.wrap { Logger.info("Will Play", "App") })
-        player.on(Event.PLAYING.value, Callback.wrap { Logger.info("Playing", "App") })
-        player.on(Event.DID_COMPLETE.value, Callback.wrap { Logger.info("Completed", "App") })
-        player.on(Event.BUFFER_UPDATE.value, Callback.wrap { bundle: Bundle? -> Logger.info("Buffer update: " + bundle?.getDouble("percentage"), "App") })
+        mediaUrl = findViewById(R.id.url_text) as EditText
+        playerState = findViewById(R.id.player_state) as TextView
 
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.container, player)
-        fragmentTransaction.commit()
+        player = Player()
 
-        player.play()
+        updatePlayerState()
+
+        Event.values().forEach { player.on(it.value, Callback.wrap { runOnUiThread { updatePlayerState() } }) }
+
+        player.on(Event.BUFFER_UPDATE.value, Callback.wrap { Logger.info("Buffer update: " + it?.getDouble("percentage"), "App") })
+
+        player.on(Event.ERROR.value, Callback.wrap {
+            val errorInfo : ErrorInfo? = it?.getParcelable(Event.ERROR.value)
+            Logger.info("Error: " + errorInfo?.code + " \"" + errorInfo?.message + "\"", "App")
+            errorInfo?.let {
+                AlertDialog.Builder(player.activity)
+                        .setMessage("Code: " + it.code + "\nMessage: \"" + it.message + "\"")
+                        .setTitle(R.string.error_dialog_title)
+                        .create()
+                        .show()
+            }
+        })
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.player, menu)
-        return true
+    fun updatePlayerState() {
+        playerState.text =
+                when (player.state) {
+                    Player.State.NONE -> "None"
+                    Player.State.PLAYING -> "Playing"
+                    Player.State.PAUSED -> "Paused"
+                    Player.State.ERROR -> "Error"
+                    Player.State.IDLE -> "Idle"
+                    Player.State.STALLED -> "Stalled"
+                }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-        if (id == R.id.action_settings) {
-            return true
+    fun playMedia(view: View) {
+        val url = mediaUrl.text.toString()
+
+
+        if (url.isNotEmpty()) {
+            hideKeyboard()
+
+            if (fragmentManager.findFragmentByTag("clappr_player") == null) {
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.add(R.id.container, player, "clappr_player")
+                fragmentTransaction.commit()
+
+                player.configure(Options(source = url, autoPlay = false))
+            } else {
+                player.load(url)
+            }
+
+            player.play()
         }
-        return super.onOptionsItemSelected(item)
+    }
+
+    private fun hideKeyboard() {
+        val inputMethodManager = this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), 0)
     }
 }
